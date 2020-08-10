4ffda7fee6d2cc02646300c800107601427ee7ed
Jørgen Jacobsen <jorgeja@gmail.com>
2020-08-10 13:50:29 +0200

    updated gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,15 +10,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-transition-link`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
