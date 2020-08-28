2ee23eb160651b023e5a625aa141c259f63cc4dc
Jørgen Jacobsen <jorgeja@gmail.com>
2020-08-28 23:28:04 +0200

    window

--- src/pages/vinner.js
+++ src/pages/vinner.js
@@ -2,8 +2,9 @@ import React, { useEffect } from "react"
 
 const DirektePage = () => {
   useEffect(() => {
-    window.location.href =
-      "https://docs.google.com/forms/d/e/1FAIpQLSd8F-qxSD1fX6tG8bXfVGwPNVu_cRRKttL5gI-Wumu9SX8oTw/viewform?usp=sf_link"
+    if (typeof window !== "undefined")
+      window.location.href =
+        "https://docs.google.com/forms/d/e/1FAIpQLSd8F-qxSD1fX6tG8bXfVGwPNVu_cRRKttL5gI-Wumu9SX8oTw/viewform?usp=sf_link"
   }, [])
   return <></>
 }
