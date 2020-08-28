7f6a771229f2504b0568b8f1d6d4a247d7882d53
Jørgen Jacobsen <jorgeja@gmail.com>
2020-08-28 23:25:30 +0200

    premie

--- src/pages/vinner.js
+++ src/pages/vinner.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 const DirektePage = () => {
-  window.location.href =
-    "https://docs.google.com/forms/d/e/1FAIpQLSd8F-qxSD1fX6tG8bXfVGwPNVu_cRRKttL5gI-Wumu9SX8oTw/viewform?usp=sf_link"
-
+  useEffect(() => {
+    window.location.href =
+      "https://docs.google.com/forms/d/e/1FAIpQLSd8F-qxSD1fX6tG8bXfVGwPNVu_cRRKttL5gI-Wumu9SX8oTw/viewform?usp=sf_link"
+  }, [])
   return <></>
 }
 
